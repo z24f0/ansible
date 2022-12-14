c2c9c6f4182707b3dd9ea973054a57f097d7f965
z24f0 <f.mahammad@yahoo.com>
2022-12-14 12:17:22 +0100

    script-present-absent.sh

---
+++ script-present-absent.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+if [ -z $1 ]
+then
+	echo "Entrer 0(present) ou 1(absent)"
+	exit 1
+else
+	choix=$1
+	if [ $choix -eq 0 ]
+	then
+		choix="present"
+		choix2="absent"
+	else
+		choix="absent"
+		choix2="present"
+	fi
+	file=/home/user1/ansible/fichiers-a-modif_sed.txt
+	nb_lignes=$(wc -l $file | cut -d' ' -f1)
+
+	cd roles | grep -r "state: $choix2" | cut -d':' -f1 | grep "roles" > $file
+
+	for i in $(seq 1 ${nb_lignes});
+	do
+		l=$(head -n$i $file| tail -n1)
+		sed -i "s/state: $choix2/state: $choix/" $l
+		echo $l
+	done
+fi
